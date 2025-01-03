53320ca3b040c6a4b8368be4ebc35523a83e09b5
AlexCS1337 <alexander.carlos.silva@gmail.com>
2025-01-03 20:15:30 +1000

    Add examples for Abstration and Polymorphism

--- src/Abstraction.cpp
+++ src/Abstraction.cpp
@@ -6,3 +6,24 @@
 // important information about data and hides unecessary information from the
 // user's perspective. Control Abstraction hides code details and provides code
 // blocks or functions to use in an application.
+#include <iostream>
+
+class Shape {
+public:
+    // Pure virtual function providing interface framework.
+    virtual void draw() = 0; 
+};
+
+class Sphere : public Shape {
+public:
+    void draw() override {
+      std::cout << "Drawing a Sphere\n"; // The actual functionality is hidden from viewer
+    }
+};
+
+int main() {
+    Shape* shape = new Sphere();
+    shape->draw();
+    delete shape;
+    return 0;
+}
--- src/Polymorphism.cpp
+++ src/Polymorphism.cpp
@@ -1,3 +1,40 @@
 /* Polymorphism */
 
-// Polymorphism allows you to use a single interface to represent different data types.
+// Polymorphism allows for methods to do different things based on the object is being used on, even if the name of the method is the same.
+//
+//
+#include <iostream>
+
+// Base class
+class Animal {
+  public:
+    void animalSound() {
+      std::cout << "The animal makes a sound \n";
+    }
+};
+
+// Derived class
+class Cat : public Animal {
+  public:
+    void animalSound() {
+      std::cout << "The cat says: nyan nyan ₍^. .^₎⟆\n";
+    }
+};
+
+// Derived class
+class Dog : public Animal {
+  public:
+    void animalSound() {
+      std::cout << "The dog says: wan wan \n";
+    }
+};
+
+int main() {
+  Animal generalAnimal;
+  Cat neko;
+  Dog inu;
+  generalAnimal.animalSound();
+  neko.animalSound();
+  inu.animalSound();
+  return 0;
+}
